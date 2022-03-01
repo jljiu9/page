93b7c049646b6cf817373c9ecf8d3edbdbe197a8
jljiu9 <jljiu@qq.com>
2022-03-01 20:38:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ let server = http.createServer(async(req,res)=>{
     if (req.url == '/' ){
         res.writeHead(200,{ 'Content-Type': 'text/plain; charset=utf-8' })
         res.end('欢迎！')
+        
     }
+    res.writeHead(200,{ 'Content-Type': 'text/plain; charset=utf-8' })
+    res.end('欢迎你啊！')
 })
 server.listen(8080,()=>cl('Listing on port 8080'))
