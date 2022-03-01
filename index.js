f9c5bb58b1b48e10c41a6ac42192512ece1352b6
jljiu9 <77003319+jljiu9@users.noreply.github.com>
2022-03-01 22:53:28 +0800

    k

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const http = require('http')
 const cl = u => console.log(u)
 let server = http.createServer(async(req,res)=>{
+    // req.url.replace('/','')
     res.writeHead(200,{ 'Content-Type': 'text/plain; charset=utf-8' })
-    res.end('欢迎你啊！')
+    res.end('欢迎你啊！'+req.url.replace('/',''))
 })
 server.listen(8080,()=>cl('nihao!'))
