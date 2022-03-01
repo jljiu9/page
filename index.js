d5f1024a5e68d9f444db2ac78bb4adbfcdc54635
jljiu9 <77003319+jljiu9@users.noreply.github.com>
2022-03-01 20:44:14 +0800

    n

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const http = require('http')
 const cl = u => console.log(u)
 let server = http.createServer(async(req,res)=>{
-    if (req.url == '/' ){
-        res.writeHead(200,{ 'Content-Type': 'text/plain; charset=utf-8' })
-        res.end('欢迎！')
-        
-    }
     res.writeHead(200,{ 'Content-Type': 'text/plain; charset=utf-8' })
     res.end('欢迎你啊！')
 })
-server.listen(8080,()=>cl('Listing on port 8080'))
+server.listen(8080,()=>cl('nihao!'))
