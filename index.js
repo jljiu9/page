22759e21728d9bff3f6fbec68a5125fc12b86f34
jljiu9 <77003319+jljiu9@users.noreply.github.com>
2022-03-02 17:08:22 +0800

    url

--- index.js
+++ index.js
@@ -11,6 +11,35 @@ let server = http.createServer(async(req,res)=>{
         res.writeHead(200,{ 'Content-Type': 'text/plain; charset=utf-8' })
         res.end('欢迎！')
     }
+    if (req.url.includes('/url=')) {
+        let urll = req.url.replace('/url=','')
+        if (urll.includes('douyin.com')){
+            dy(req,res)
+            return
+        }
+        cl('正在访问：'+parseUrl(urll))
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            // headless: false,
+            // slowMo: 10,
+        });
+        const page = (await browser.pages())[0];
+        await page.setViewport({width: 1920, height: 1080});
+        await page.goto(parseUrl(urll));
+        await page.addScriptTag({path:'src/jquery.js'})
+        await page.addStyleTag({path:'src/font.css'})
+        await page.evaluate(() => {
+            $('*').css('font-family','cnFont')
+        })
+        await page.screenshot().then(function(buffer) {
+            // res.setHeader('Content-Disposition', 'attachment;filename="' + urll + '.png"')   //下载图片
+            res.setHeader('Content-Type', 'image/png')
+            res.end(buffer)
+        });
+        await browser.close();
+        clearInterval(ttime)
+        cl('访问完成！'+'用时：'+(st*(0.01)).toFixed(2)+' s')
+    }
     // req.url.replace('/','')
     // res.writeHead(200,{ 'Content-Type': 'text/plain; charset=utf-8' })
     // res.end('欢迎你啊！'+req.url.replace('/',''))
