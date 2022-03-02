d763641684b3a5cfdcb0838e7563315b7db50ad1
jljiu9 <77003319+jljiu9@users.noreply.github.com>
2022-03-02 16:48:42 +0800

    people

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const http = require('http')
 const cl = u => console.log(u)
+let foo = 0
 let server = http.createServer(async(req,res)=>{
+    let st = 0
+    let ttime = setInterval(() => {
+        st++
+    }, 10)
+    if (req.url == '/') {
+        cl('第 ' + ++foo + ' 人访问！')
+        res.writeHead(200,{ 'Content-Type': 'text/plain; charset=utf-8' })
+        res.end('欢迎！')
+    }
     // req.url.replace('/','')
-    res.writeHead(200,{ 'Content-Type': 'text/plain; charset=utf-8' })
-    res.end('欢迎你啊！'+req.url.replace('/',''))
+    // res.writeHead(200,{ 'Content-Type': 'text/plain; charset=utf-8' })
+    // res.end('欢迎你啊！'+req.url.replace('/',''))
 })
 server.listen(8080,()=>cl('nihao!'))
